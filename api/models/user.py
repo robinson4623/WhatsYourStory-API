d48ba2625684255961d620c2556966522b84c460
robinson4623@gmail.com <robinson4623@gmail.com>
2022-05-26 09:00:39 -0600

    Update

--- api/models/user.py
+++ api/models/user.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
     # The create_user method is passed:
     # self:      All methods in Python receive the class as the first argument
     # email:     Because we want to be able to log users in with email
-    #            instead of username (Django's default behavior)
+    #            instead of username (Django's default behavior) ...
     # password:  The password has a default of None for validation purposes.
     #            This ensures the proper error is thrown if a password is
     #            not provided.
