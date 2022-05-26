f2d80aef69a557c1f731cafd31d41196665c5ac6
robinson4623@gmail.com <robinson4623@gmail.com>
2022-05-26 08:06:13 -0600

    'Update' curl script modified.

--- curl-scripts/stories/update.sh
+++ curl-scripts/stories/update.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ID="14"
+
+TOKEN="cf4c624cd1ef1444c95e390881212d5bc5a29fa1"
+
+TITLE="Updated Title"
+
+STORY="Updated story that is a little longer in length just to test it out and see how things will look."
+
+
 
 curl "http://localhost:8000/stories/${ID}/" \
   --include \
@@ -7,9 +16,8 @@ curl "http://localhost:8000/stories/${ID}/" \
   --header "Authorization: Token ${TOKEN}" \
   --data '{
     "story": {
-      "name": "'"${NAME}"'",
-      "color": "'"${COLOR}"'",
-      "ripe": "'"${RIPE}"'"
+      "title": "'"${TITLE}"'",
+      "story": "'"${STORY}"'"
     }
   }'
 
